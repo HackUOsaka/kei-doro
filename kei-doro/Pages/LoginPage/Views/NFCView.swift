babd4d9ecd2236b9d2b72a0cb5bd21ee0cb67de6
saki0411 <chipdale411@gmail.com>
2024-03-07 08:13:13 +0900

    create: create-directory#6

---
+++ kei-doro/Pages/LoginPage/Views/NFCView.swift
@@ -0,0 +1,22 @@
+//
+//  LoginView.swift
+//  kei-doro
+//
+//  Created by saki on 2024/03/06.
+//
+
+import SwiftUI
+
+struct NFCView: View {
+    var body: some View {
+        Button(action: {
+            
+        }, label: {
+            Text("NFC読み込み")
+        })
+    }
+}
+
+#Preview {
+    NFCView()
+}
